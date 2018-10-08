7e44fd12a094c28744e8f8c9bd77d15f62a698a2
unknown <alessanderrubiato@yahoo.com>
2018-10-08 17:16:41 +0800

    nothing too fancy

--- register-user.php
+++ register-user.php
@@ -11,25 +11,33 @@ require 'include/theme.php';
 	<div class = "container-fluid">
 		<div class = "card col-lg-6 mx-auto mt-5">
 			<div class = "card-body">
+			<form method = "post" action = "functions/user-function.php">
 				<div class = "card-title" align="center">Create an account</div>
 					<hr>
 				<div class = "form-group">
-					<input type = "text" class = "form-control" name = "fname" placeholder="Firstname">
+					<input type = "text" class = "form-control" name = "UP_FNAME" placeholder="Firstname">
 				</div>
 				<div class = "form-group">
-					<input type = "text" class = "form-control" name = "lname" placeholder="Lastname">
+					<input type = "text" class = "form-control" name = "UP_LNAME" placeholder="Lastname">
 				</div>
 				<div class = "form-group">
-					<input type = "text" class = "form-control" name = "email" placeholder="Email">
+					<select class = "form-control">
+						<option selected = "selected" hidden>--SELECT--</option>
+						<option value = "Male">Male</option>
+						<option value = "Female">Female</option>
+					</select>
 				</div>
 				<div class = "form-group">
-					<input type = "date" class = "form-control">
+					<input type = "text" class = "form-control" name = "UP_EMAIL" placeholder="Email">
 				</div>
 				<div class = "form-group">
-					<input type = "text" class = "form-control" name = "uname" placeholder="Username">
+					<input type = "date" class = "form-control" name = "UP_BDATE">
 				</div>
 				<div class = "form-group">
-					<input type = "password" class = "form-control" name = "pwd" placeholder="Password">
+					<input type = "text" class = "form-control" name = "UNAME" placeholder="Username">
+				</div>
+				<div class = "form-group">
+					<input type = "password" class = "form-control" name = "PWD" placeholder="Password">
 				</div>
 				<div class = "form-group">
 						<button class = "btn btn-primary btn-block" type = "submit" name = "register-user">Register</button>
@@ -39,6 +47,7 @@ require 'include/theme.php';
 				</div>
 
 		</div>
+	</form>
 	</div>
 </body>
 </html>
