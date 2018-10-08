1ee6881e51d61eb334f284f64aa05372e3be2ae8
unknown <alessanderrubiato@yahoo.com>
2018-10-08 17:15:51 +0800

    code broken, needs fix

--- functions/user-function.php
+++ functions/user-function.php
@@ -3,26 +3,41 @@
 require 'config-db.php';
 
 
+if(isset($_POST['register-user'])) {
+    
 
-if(isset($_POST['register-user']) {
-
-$UP_FNAME = isset($_POST['UP_FNAME']) ? $_POST['UP_FNAME']:"";
-$UP_LNAME = isset($_POST['UP_LNAME']) ? $_POST['UP_LNAME']:"";
-$UP_GDR = isset($_POST['UP_GDR']) ? $_POST['UP_GDR']:"";
-$UP_EMAIL = isset($_POST['UP_EMAIL']) ? $_POST['UP_EMAIL']:"";
-$UP_BDATE = isset($_POST['UP_BDATE']) ? $_POST['UP_BDATE']:"";
-$UP_ADD = isset($_POST['UP_FNAME']) ? $_POST['UP_FNAME']:"";
-$UP_CN = isset($_POST['UP_CN']) ? $_POST['UP_CN']:"";
-$UNAME = isset($_POST['UNAME']) ? $_POST['UNAME']:"";
-$PWD = isset($_POST['PWD']) ? $_POST['PWD']:"";
-
-$HASHED_PWD = password_hash($PWD,PASSWORD_DEFAULT);
+    $UNAME = isset($_POST['UNAME']) ? $_POST['UNAME'] : "";
+    $PWD = isset($_POST['PWD']) ? $_POST['PWD'] : "";
+    $UP_FNAME = isset($_POST['UP_FNAME']) ? $_POST['UP_FNAME'] : "";
+    $UP_LNAME = isset($_POST['UP_LNAME']) ? $_POST['UP_LNAME'] : "";
+    $UP_GDR = isset($_POST['UP_GDR']) ? $_POST['UP_GDR'] : "";
+    $UP_EMAIL = isset($_POST['UP_EMAIL']) ? $_POST['UP_EMAIL'] : "";
+    $UP_BDATE = isset($_POST['UP_BDATE']) ? $_POST['UP_BDATE'] : "";
 
 
-$sql = "INSERT INTO user_profile (U_ID,UP_FNAME,UP_LNAME,UP_GDR,UP_EMAIL,UP_BDATE,UP_ADD,UP_CN) "
+// $HASHED_PWD = password_hash($PWD,PASSWORD_DEFAULT);
 
 
 
+
+    $sql1 = "INSERT INTO user (UNAME,PWD) VALUES ('$UNAME','$PWD');";
+
+    if(mysqli_query($sql1)) {
+        $latest_id = LAST_INSERT_ID();
+
+        $sql2 = "INSERT INTO user_profile (U_ID,UP_FNAME,UP_LNAME,UP_GDR,UP_EMAIL,UP_BDATE) VALUES ('$latest_id','$UP_FNAME','$UP_LNAME','$UP_GDR','$UP_EMAIL','$UP_BDATE')";
+        
+        if(mysqli_query($sql2)) {
+            echo "Success";
+        }
+    }
+
+header("location: ../login-user.php");
+echo "Succesfully created an account!";
+
+
+} else {
+echo "There's an error while inserting data to database";
 }
 
 ?>
