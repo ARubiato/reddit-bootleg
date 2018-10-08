430563cb6cd205514c8521bb4789488c969cea1f
unknown <alessanderrubiato@yahoo.com>
2018-10-08 17:15:22 +0800

    added an if condition

--- functions/config-db.php
+++ functions/config-db.php
@@ -7,4 +7,9 @@ $dbase = 'reddit-bootleg';
 
 $conn = mysqli_connect($server,$username,$password,$dbase) or die ("Error in connecting to database" . mysqli_error());
 
+if(!$conn) {
+    $conn = mysqli_connect('localhost','root','','reddit-bootleg') or die ("Error in connecting to database" . mysqli_error());
+}
+
+
 ?>
